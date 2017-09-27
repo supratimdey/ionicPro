936fc5fe57ec21939f1878c00e1e6d76533b7644
Supratim <supratimdey@outlook.com>
2017-09-27 23:30:46 +0530

    First Commit of the software developed in 2 days

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {DetailPage} from '../pages/detail/detail';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailPage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    DetailPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { DetailPage } from '../detail/detail';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  posts: any[];
 
+  constructor(public navCtrl: NavController , public http: Http) {
+
+    this.http.get('https://www.reddit.com/r/gifs/new/.json?limit=30').map(
+      res => res.json()).subscribe( data => {
+          this.posts = data.data.children;
+        }, err => { console.log("Sorry Erroer!")});
+
+    // this.items =[];
+    // for(let i =0; i < 10; i++) {
+    //   this.items.push({text: 'Item '+ i,
+    //   id:i });
+    // }
   }
 
+  // itemSelected(item){
+  // //  alert(item.text);
+  //   this.navCtrl.push(DetailPage, {
+  //     item: item
+  //   });
+  // }
 }
